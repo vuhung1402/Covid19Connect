13dbc54ee15a28ae5f6a9ac059680bc920e29fb6
vuhung1402 <badao867@gmail.com>
2022-10-29 14:16:29 +0700

    second

--- insertAWSDB_AuroraMySQL.py
+++ insertAWSDB_AuroraMySQL.py
@@ -3,7 +3,7 @@ import MySQLdb
 
 
 #Tạo kết nối với RDS Aurora MySQL và thêm dữ liệu vào
-conn = MySQLdb.connect(host='database-aurora.cluster-cpb15cjrvey3.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
+conn = MySQLdb.connect(host='database-aurora.cluster-cdn8owrkeukl.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
--- insertAWSDB_PostgresQL.py
+++ insertAWSDB_PostgresQL.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 #Tạo kết nối với RDS PostgreSQL và thêm dữ liệu vào
-conn = psycopg2.connect(database='covid19_postgre', user='postgres', password='12345678', host='database-postgre.cpb15cjrvey3.us-east-1.rds.amazonaws.com', port='5432')
+conn = psycopg2.connect(database='covid19_postgres', user='postgres', password='12345678', host='database-postgresql.cdn8owrkeukl.us-east-1.rds.amazonaws.com', port='5432')
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
